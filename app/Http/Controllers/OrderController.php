58a71e06242729fcda0d49c60d85d4fe0873687c
Chris Mcintosh <chris@mcintosh.io>
2022-11-09 11:27:59 -0500

    fix order request param on index

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -8,8 +8,8 @@ use App\Models\Order;
 class OrderController extends Controller
 {
     public function index(Request $request) {
-        if($request->has('user_id')) {
-            return Order::where('user_id', $request->input('user_id'))->get()->toJson();
+        if($request->has('user')) {
+            return Order::where('user_id', $request->input('user'))->get()->toJson();
         }
         return Order::all()->toJson();
     }
