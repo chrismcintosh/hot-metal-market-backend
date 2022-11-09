661448226ff2c02bb1e710405acb9f181bd8449b
Chris Mcintosh <chris@mcintosh.io>
2022-11-09 11:37:32 -0500

    dont show payment intent generated in user facing orders list

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -9,9 +9,13 @@ class OrderController extends Controller
 {
     public function index(Request $request) {
         if($request->has('user')) {
-            return Order::where('user_id', $request->input('user'))->get()->toJson();
+            return Order::where('user_id', $request->input('user'))
+                ->where('status', '!=', 'pi_generated')
+                ->get()
+                ->toJson();
         }
-        return Order::all()->toJson();
+        return Order::where('status', '!=', 'pi_generated')
+            ->toJson();
     }
 
     public function show(Order $order) {
